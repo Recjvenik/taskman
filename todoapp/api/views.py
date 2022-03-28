817a098bfd9c6169a7251f73fff5b8365c695022
yogeshsharma0 <yogesh.sharma@careers360.com>
2022-03-28 14:27:13 +0530

    second commit

---
+++ todoapp/api/views.py
@@ -0,0 +1,77 @@
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from base.models import Task, User
+from .serializers import TaskSerializer, UserSeralizer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'tasks': reverse('task-list', request=request, format=format)
+    })
+
+
+@api_view(['GET', 'POST'])
+def task_list_view(request, format=None):
+
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True, context={'request': request})
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['DELETE'])
+def task_delete(request, pk, format=None):
+    try:
+        task = Task.objects.get(id=pk)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def user_list_view(request, format=None):
+    """
+    This viewset automatically provides `list` actions.
+    """
+
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSeralizer(users, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def user_detail(request, pk, format=None):
+    """
+    This viewset automatically provides `detail` actions.
+    """
+
+    try:
+        user = User.objects.get(id=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'GET':
+        serializer = UserSeralizer(user, context={'request': request})
+        return Response(serializer.data)
+
+
+
+
+
+
